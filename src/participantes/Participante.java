1f03ea6428c1019d1078a5371b17345c71fbe49a
WesRM <wruizm@est.utn.ac.cr>
2026-06-28 07:39:01 +0200

    Se crea la clase participante
    Se le agrega a la clase los metodos de acceso, constructor y toString

--- src/participantes/Participante.java
+++ src/participantes/Participante.java
@@ -4,10 +4,47 @@
  */
 package participantes;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Wesley Ruiz
  */
 public class Participante {
+    private String nombre;
+    private String id;
+    private int telefono;
+    private LocalDate fechaNacimiento;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public Participante(String nombre, String id, int telefono, LocalDate fechaNacimiento) {
+        this.nombre = nombre;
+        this.id = id;
+        this.telefono = telefono;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Participante" + "\nombre=" + nombre + "\nid=" + id + "\ntelefono=" + telefono + "\nfechaNacimiento=" + fechaNacimiento;
+    }
+    
+    
+    
     
 }
